ccc21212a9f95b878a66d91b6ed41d5e725ba73f
kesavamuthu <kesavamuthu77@gmail.com>
2020-08-07 02:00:51 +0530

    url check

--- models/db.config.js
+++ models/db.config.js
@@ -1,5 +1,6 @@
 const env = require("dotenv").config();
 const mongoose = require("mongoose");
-
+console.log(process.env.MONGO_URL);
 let url = process.env.MONGO_URL.replace("***", "mainProject");
+console.log(url);
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
